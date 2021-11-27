f893a34aaac4e54caa5597d68ede469388317bd2
FedericoCaspani <federico1.caspani@mail.polimi.it>
2021-11-27 11:59:00 +0100

    first idea

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Dropout
 
 # if the dataset is still to be formatted, turn this to true
-DATASET_TO_FORMAT = True
+DATASET_TO_FORMAT = False
 
 
 # this function checks for format issues and, if it founds them, resolves and re-write the correct dataset.
@@ -31,10 +33,8 @@ def get_right_data():
         new_dataset = dates.to_frame().join(data)
         new_dataset.to_csv("deliveries/dataset/bitcoin_price_Training - Training.csv")
 
-    print("before: "+str(data))
     scaler = MinMaxScaler()
     data = scaler.fit_transform(data)
-    print("after: "+str(data))
 
     return dates, data
 
@@ -42,10 +42,42 @@ def get_right_data():
 def run():
 
     test_date, training_data = get_right_data()
+    X_train = []
+    Y_train = []
 
-    print("data type:"+str(type(training_data)))
-    print("dates type: "+str(type(test_date)))
+    for i in range(60, training_data.shape[0]):
+        X_train.append(training_data[i - 60:i])
+        Y_train.append(training_data[i, 0])
+
+    X_train, Y_train = np.array(X_train), np.array(Y_train)
+
+    # Initialize the RNN
+    model = Sequential()
+    model.add(LSTM(units=70, activation='relu', return_sequences=True, input_shape=(X_train.shape[1], 7)))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=80, activation='relu', return_sequences=True))
+    model.add(Dropout(0.3))
+    model.add(LSTM(units=100, activation='relu', return_sequences=True))
+    model.add(Dropout(0.4))
+    model.add(LSTM(units=140, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(units=1))
+    model.summary()
+
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    history = model.fit(X_train, Y_train, epochs=20, batch_size=50, validation_split=0.1)
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs = range(len(loss))
+    plt.figure()
+    plt.plot(epochs, loss, 'b', label='Training loss')
+    plt.plot(epochs, val_loss, 'r', label='Validation loss')
+    plt.title("Training and Validation Loss")
+    plt.legend()
+    plt.show()
 
 
 if __name__ == '__main__':
+    print(np.__version__)
     run()
